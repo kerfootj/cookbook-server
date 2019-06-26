c27ecef31402799d5c5b715c89ef47b3f278924e
Joel Kerfoot <joelkerfoot@gmail.com>
2019-06-25 20:29:16 -0700

    fixed ports

--- index.js
+++ index.js
@@ -5,4 +5,7 @@ const app = express();
 
 app.get('/api/recipes', cors(), (req, res) => res.json(data));
 
-app.listen(process.env.PORT || 8080, () => console.log(`listening on port: ${port}`));
+var server = app.listen(process.env.PORT || 8080, () => {
+	var port = server.address().port;
+	console.log('App now running on port', port);
+});
