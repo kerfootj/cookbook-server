87e54ce6389c26800981927cf7050d63e858dbdc
Joel Kerfoot <joelkerfoot@gmail.com>
2019-07-06 20:48:40 -0700

    added mongoose-auto-increment

--- models/recipe.model.js
+++ models/recipe.model.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const autoIncrement = require('mongoose-auto-increment');
 
 mongoose.connect(
 	'mongodb+srv://betty:Peach-p!3@cookbook-db-6mqam.mongodb.net/cookbook?retryWrites=true&w=majority',
 	{ useNewUrlParser: true }
 );
+
+autoIncrement.initialize(mongoose.connection);
 
 const RecipeSchema = Schema({
 	title: {
@@ -12,16 +15,23 @@ const RecipeSchema = Schema({
 		required: true
 	},
 	description: String,
+	ingredients: {
+		type: [String],
+		required: true
+	},
+	instructions: {
+		type: [String],
+		required: true
+	},
 	image: {
 		id: {
-			type: String,
-			required: true
+			type: String
 		},
 		deleteHash: {
-			type: String,
-			required: true
+			type: String
 		}
 	}
 });
 
+RecipeSchema.plugin(autoIncrement.plugin, 'Recipe');
 module.exports = mongoose.model('Recipe', RecipeSchema);
