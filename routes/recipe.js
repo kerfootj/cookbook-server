335e6a692c1e22506a029589a9370aed8fa384fc
Joel Kerfoot <joelkerfoot@gmail.com>
2019-07-07 20:51:42 -0700

    added a route to get a specific recipe

--- routes/recipe.js
+++ routes/recipe.js
@@ -10,6 +10,16 @@ router.get('/recipe', (req, res) => {
 		.catch(error => res.status(500).json(error));
 });
 
+router.get('/recipe/:recipeId', (req, res) => {
+	RecipeModel.findOne({ _id: req.params.recipeId })
+		.then(document => {
+			res.json(document);
+		})
+		.catch(error => {
+			res.status(500).json(error);
+		});
+});
+
 router.post('/recipe', (req, res) => {
 	const model = new RecipeModel(req.body);
 	model
@@ -18,6 +28,7 @@ router.post('/recipe', (req, res) => {
 			res.json(document);
 		})
 		.catch(error => {
+			console.log(error);
 			res.status(500).json(error);
 		});
 });
