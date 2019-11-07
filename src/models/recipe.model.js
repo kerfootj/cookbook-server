e0934356bcf0189a9b2313003e8cb5bc50dbed2a
Joel Kerfoot <joelkerfoot@gmail.com>
2019-11-06 20:44:18 -0800

    reorganized files

---
+++ src/models/recipe.model.js
@@ -0,0 +1,67 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const autoIncrement = require('mongoose-auto-increment');
+
+mongoose.connect(
+	`mongodb+srv://${process.env.MONGO_DB_USER}:${
+		process.env.MONGO_DB_PASSWORD
+	}@cookbook-db-6mqam.mongodb.net/cookbook?retryWrites=true&w=majority`, {
+		useNewUrlParser: true
+	}
+);
+
+autoIncrement.initialize(mongoose.connection);
+
+const RecipeSchema = Schema({
+	_id: Number,
+	title: {
+		type: String,
+		required: true
+	},
+	description: String,
+	ingredients: {
+		type: [String],
+		required: true
+	},
+	instructions: {
+		type: [String],
+		required: true
+	},
+	images: [{
+		id: String,
+		deleteHash: String,
+	}],
+	tags: [String],
+	rating: Number,
+	reviews: [String],
+	prep: {
+		type: String,
+		required: true
+	},
+	cook: {
+		type: String,
+		required: true
+	},
+	ready: {
+		type: String,
+		required: true
+	},
+	servings: {
+		type: Number,
+		required: true
+	},
+	private: {
+		type: Boolean,
+		required: true
+	},
+	uid: {
+		type: String,
+		required: true
+	},
+	madeit: Number,
+	likes: Number
+});
+
+RecipeSchema.plugin(autoIncrement.plugin, 'Recipe');
+module.exports = mongoose.model('Recipe', RecipeSchema);
---
+++ src/routes/user.js
@@ -0,0 +1,43 @@
+const UserModel = require('../models/user.model');
+const express = require('express');
+const router = express.Router();
+
+router.get('/user', (req, res) => {
+	UserModel.find({})
+		.then(document => {
+			res.json(document);
+		})
+		.catch(error => res.status(500).json(error));
+});
+
+router.get('/user/:uid', (req, res) => {
+	UserModel.findOne({
+			uid: req.params.uid
+		})
+		.then(document => {
+			res.json(document);
+		})
+		.catch(error => {
+			res.status(500).json(error);
+		});
+});
+
+router.post('/user', (req, res) => {
+	const query = {
+			'uid': req.body.uid
+		};
+	const update = req.body;
+	const options = {
+			upsert: true,
+			new: true,
+			setDefaultsOnInsert: true
+		};
+	UserModel.findOneAndUpdate(query, update, options, (error, document) => {
+		if (error) {
+			return res.status(500).json(error);
+		}
+		return res.json(document);
+	});
+});
+
+module.exports = router;
